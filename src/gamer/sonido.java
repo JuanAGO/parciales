2e18774604bff17094cc3d0752b496544aa506c2
juanAngel <angeloviedo1111111@gmail.com>
2020-05-06 14:55:46 -0500

    Parcial Segundo

---
+++ src/gamer/sonido.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamer;
+
+import java.applet.AudioClip;
+
+/**
+ *
+ * @author evely
+ */
+class sonido {
+
+    void setVisible(boolean a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
